26fd4a0417a21d6c360fc087621411056966bc33
wasaw <merenkovalexander@gmail.com>
2022-10-06 16:12:50 +0300

    Создан главный экран

--- LessonGit/LessonGit/ViewController.swift
+++ LessonGit/LessonGit/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    MARK: - Properties
+    
+    private let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Добро пожаловать"
+        return label
+    }()
 
+//    MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
-
-
+    
+//    MARK: - Helpers
+    private func configureUI() {
+        view.addSubview(welcomeLabel)
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
+        welcomeLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        view.backgroundColor = .yellow
+    }
 }
 
