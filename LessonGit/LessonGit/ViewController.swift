19fbb883599086b9926f2d54e3333542a2b21564
wasaw <merenkovalexander@gmail.com>
2022-10-10 16:42:53 +0300

    Добавлена кнопка

--- LessonGit/LessonGit/ViewController.swift
+++ LessonGit/LessonGit/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
         label.text = "Добро пожаловать"
         return label
     }()
+    
+    private let button: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Кнопка", for: .normal)
+        btn.backgroundColor = .red
+        return btn
+    }()
 
 //    MARK: - Lifecycle
     
@@ -34,6 +41,13 @@ class ViewController: UIViewController {
         welcomeLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
         
         view.backgroundColor = .yellow
+        
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 25).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 170).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 35).isActive = true
     }
 }
 
